aa7fe58b430ea02e1fc4ca9c434524d2cdd855cb
KriTeRiuM0 <saboualam3@gmail.com>
2025-04-20 18:01:24 +0100

    Updated project with latest changes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+
+export const routes: Routes = [
+    { path: 'register', component: SignupComponent }
+];
